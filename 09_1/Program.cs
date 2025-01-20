863b4055a8ae686390180b2122269bc0a63eb363
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2025-01-20 17:01:36 +0900

    終わり

---
+++ 09_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ItemTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item("つるはし");
+
+            item.Use();
+
+            //一時停止
+            Console.ReadLine();
+
+        }
+    }
+}
