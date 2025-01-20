d2e4c1c0c5e1bc72022b0f2eb0b03c056995e8ce
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2025-01-20 17:42:31 +0900

    ていせい

--- 09_1/Player.cs
+++ 09_1/Player.cs
@@ -11,47 +11,19 @@ namespace PlayerTest
     {
         //フィールド
         private string name;
-        private int level;
 
         private Item item;
 
         //メソッド
         //コンストラクタ
-        public Player(string name, int level)
+        public Player(string name)
         {
             this.name = name;
-            this.level = level;
         }
 
         public Player(Item item)
         {
             this.item = item;
-        }
-
-        //攻撃する
-        public void Attack()
-        {
-            Console.WriteLine("攻撃!!");
-        }
-        //防御する
-        public void Defense()
-        {
-            Console.WriteLine("防御した!!");
-        }
-        //レベルアップする
-        public void LevelUP()
-        {
-            level++;
-        }
-        //名前を聞く
-        public string GetName()
-        {
-            return name;
-        }
-        //レベルを聞く
-        public int GetLevel()
-        {
-            return level;
         }
 
         //アイテムを使う
