0b6984c372edb9722fce0138ab161b4d2fc98dd2
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2025-01-20 17:59:50 +0900

    おわり

--- 09_1/Player.cs
+++ 09_1/Player.cs
@@ -10,27 +10,13 @@ namespace PlayerTest
     internal class Player
     {
         //フィールド
-        private string name;
-
-        private string item;
-        List<Item> items;
+        private List<Item> items;
 
         //メソッド
         //コンストラクタ
-        public Player(string name)
-        {
-            this.name = name;
-        }
-
         public Player()
         {
             items = new List<Item>();
-        }
-
-        //アイテムを使う
-        public void UseItem()
-        {
-            Console.WriteLine("{0}を使った", item);
         }
 
         //アイテムを受け取る
